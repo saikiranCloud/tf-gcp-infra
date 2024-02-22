979b908409ed3d9bd9bdfa1f2a40bad5d49b4d6e
Saikiran8 <kollojusaikiran@gmail.com>
2024-02-21 21:53:41 -0500

    added vm

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "project_id" {
-    description = "The ID of the GCP project"
+  description = "The ID of the GCP project"
 }
 variable "region" {
   description = "The region for the resources"
@@ -10,11 +10,11 @@ variable "vpc_name" {
 }
 
 variable "auto_create_subnetworks" {
-   description = "Boolean value to not create subnetworks"
+  description = "Boolean value to not create subnetworks"
 }
 
 variable "delete_default_routes_on_create" {
-   description = "Boolean value to delete default routes on create"
+  description = "Boolean value to delete default routes on create"
 }
 
 variable "webapp_subnet_name" {
@@ -45,10 +45,38 @@ variable "route_range" {
   description = "Range for the webapp subnet route"
 }
 
-variable next_hop_gateway {
+variable "next_hop_gateway" {
   description = "value for next_hop_gateway"
 }
 
-variable application_port {
+variable "application_port" {
   description = "value for next_hop_gateway"
+}
+
+variable "instance_name" {
+  description = "value for instance"
+}
+
+variable "machine_type" {
+  description = "value for machine_type"
+}
+
+variable "zone" {
+  description = "value for zone"
+}
+
+variable "image_name" {
+  description = "value for image_name"
+}
+variable "size" {
+  description = "value for size"
+}
+variable "type" {
+  description = "value for size"
+}
+variable "priority_allow" {
+  description = "value for allow"
+}
+variable "priority_deny" {
+  description = "value for priority_deny"
 }
